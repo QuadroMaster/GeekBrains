edf644474d86e3656ad04d06b81347cbd0077671
QuadroMaster <89321526+QuadroMaster@users.noreply.github.com>
2024-01-29 18:39:20 +0300

    git commit -m 'выполнил 1 задание 4 семинара'

---
+++ Intro_programming_languages/homeworks/lesson_4_seminar/1_task/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 1: Напишите программу, которая бесконечно запрашивает целые числа с консоли. Программа завершается при вводе символа ‘q’ или при вводе числа, сумма цифр которого чётная.
+
+//using System.Globalization;
+
+
+string inputText;
+while(true) {
+    Console.Write("Введите целое число: ");
+    inputText = Console.ReadLine()!;
+    if (inputText == "q")
+        {
+            Console.Write("Выход из цикла");
+            return;
+        }
+    //number = Convert.ToInt32(inputText);
+    int sum = 0;
+    foreach (char i in inputText)
+        {   
+            sum += Convert.ToInt32(i);
+        }
+    if (sum % 2 == 0)
+    {   
+        Console.WriteLine(sum % 2);
+        Console.Write("Введено число, сумма цифр которого чётная, выход из цикла");
+        return;
+    }
+    
+}
