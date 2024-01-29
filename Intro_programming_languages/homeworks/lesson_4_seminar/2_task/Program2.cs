f9f8fe3097e4224546fbb87c26fe2d1051c2e8d7
QuadroMaster <89321526+QuadroMaster@users.noreply.github.com>
2024-01-29 19:46:02 +0300

    add 2 and 3 homeworks

---
+++ Intro_programming_languages/homeworks/lesson_4_seminar/2_task/Program2.cs
@@ -0,0 +1,33 @@
+﻿int[] CreateArray(int size)
+    {
+        int[] arr = new int[size];
+        Random rnd = new Random();
+        for(int i = 0; i < size; i++)
+        {
+            
+            arr[i] = rnd.Next(100, 1000);
+            
+        }
+        for (int j = 0; j < size; j++)
+        {
+            Console.WriteLine(arr[j]);
+        }
+        return arr;
+    }
+
+int EvenCounter (int[] array)
+    {   
+        int counter = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] % 2 == 0)
+            counter += 1;
+        } 
+        Console.WriteLine($"\nКоличество четных чисел в массиве: {counter}");
+        return counter;
+    }
+
+
+EvenCounter(CreateArray(5));
+
+//Console.Write(x);
