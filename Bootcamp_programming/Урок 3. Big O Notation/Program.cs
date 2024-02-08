640e5ac4e24267d192f8db8a87dff78f4f3d0e42
Svyatoslav Vyugin <vjugin.kibernetik@yandex.ru>
2024-02-08 22:46:23 +0300

    added Bootcamp_programming

---
+++ Bootcamp_programming/Урок 3. Big O Notation/Program.cs
@@ -0,0 +1,15 @@
+﻿//int n = 5;
+//int[] array = new int[n];
+//Console.WriteLine("ВВЕДИТЕ СВОИ ЧИСЛА: ");
+//for (int i = 0; i < n; i++)
+ //   array[i] = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("[" + string.Join(", ", array) + "]");
+//Console.WriteLine(array[3]);
+// Сложность алгоритма О(1) - количество действий, чтобы узнать конечный результат (не больше этого количества) ВАЖНО!!!
+// [4, 5, 3, 1, 2]
+// O(n)
+// [1, 2, 3, 4, 5] = O(n * log n)
+// ((5 + 1)/2) * 5 = O(1)
+// n < n * log(n) + 1
+
+
